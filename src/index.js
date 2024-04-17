c165ea8af964c90721a548528222ca626a84a6e2
Michael <oliengo.michael@gmail.com>
2024-04-17 12:03:43 +0300

    complete tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTask(e.target.new_task_description.value)
+  })
+})
+
+function buildTask(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener(`click`, handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
